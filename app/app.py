15d38ef59168f548bb85849d667c3447d3e1eae0
Pablo <al07094461@tecmilenio.mx>
2026-04-18 09:46:17 -0600

    App corregida y funcionando al 100%

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(_name_)
+app = Flask(__name__)  
 
 @app.route('/')
 def hello_world():
-    return '¡Pipeline CI/CD funcionando con Docker y LocalStack!'
+    return 'Hola Mundo desde Docker y Flask!'
 
-if _name_ == '_main_':
+if __name__ == '__main__':  
     app.run(host='0.0.0.0', port=5000)
